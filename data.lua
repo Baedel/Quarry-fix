fa71db076b7c5b1da3249ee63f119db9a8810e1b
Baedel <Baedel@users.noreply.github.com>
2018-03-11 13:50:31 +0100

    Initial file upload
    Initial file upload

---
+++ data.lua
@@ -0,0 +1,6 @@
+if data.raw.item["quarry"] == nil then
+    require("prototypes.entity")
+    require("prototypes.item")
+    require("prototypes.recipe")
+    require("prototypes.technology")
+end
---
+++ prototypes/technology.lua
@@ -0,0 +1,12 @@
+table.insert(data.raw["technology"]["advanced-material-processing"].effects,
+  {
+    type = "unlock-recipe",
+    recipe = "quarry-mk2"
+  }
+)
+table.insert(data.raw["technology"]["advanced-material-processing-2"].effects,
+  {
+    type = "unlock-recipe",
+    recipe = "quarry-mk3"
+  }
+)
